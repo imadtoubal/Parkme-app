840c11225ae3dc3c7643a62aed9f86523ed776ea
Imad Eddine Toubal <Imad Eddine Toubal>
2017-04-11 22:24:21 +0100

    removed some debugging console thingy

--- public/js/main.js
+++ public/js/main.js
@@ -50,14 +50,14 @@ window.initMap = function () {
           styles: [{ featureType: "all", elementType: "labels.text.fill", stylers: [{ color: "#ffffff" }] }, { featureType: "all", elementType: "labels.text.stroke", stylers: [{ color: "#000000" }, { lightness: 13 }] }, { featureType: "administrative", elementType: "geometry.fill", stylers: [{ color: "#000000" }] }, { featureType: "administrative", elementType: "geometry.stroke", stylers: [{ color: "#144b53" }, { lightness: 14 }, { weight: 1.4 }] }, { featureType: "landscape", elementType: "all", stylers: [{ color: "#08304b" }] }, { featureType: "poi", elementType: "geometry", stylers: [{ color: "#0c4152" }, { lightness: 5 }] }, { featureType: "road.highway", elementType: "geometry.fill", stylers: [{ color: "#000000" }] }, { featureType: "road.highway", elementType: "geometry.stroke", stylers: [{ color: "#0b434f" }, { lightness: 25 }] }, { featureType: "road.arterial", elementType: "geometry.fill", stylers: [{ color: "#000000" }] }, { featureType: "road.arterial", elementType: "geometry.stroke", stylers: [{ color: "#0b3d51" }, { lightness: 16 }] }, { featureType: "road.local", elementType: "geometry", stylers: [{ color: "#000000" }] }, { featureType: "transit", elementType: "all", stylers: [{ color: "#146474" }] }, { featureType: "water", elementType: "all", stylers: [{ color: "#021019" }] }]
 
         };
-        $.getJSON('/all', function (data) {
-          all = data;
-          str = 'insert into maps.parkings\n (name, lat, lng, description, a_lots, t_lots, pin, price)\n'
-          for (var i = 0; i < all.length ; i++){
-            str += '("' + all[i].name + '",' + all[i].lat + ',' + all[i].lng + ',"' + all[i].description + '",' + all[i].a_lots + ',' + all[i].t_lots + ',"' + all[i].pin + '",' + all[i].price + '), \n';
-          }
-          console.log(str);
-        });
+        // $.getJSON('/all', function (data) {
+        //   all = data;
+        //   str = 'insert into maps.parkings\n (name, lat, lng, description, a_lots, t_lots, pin, price)\n'
+        //   for (var i = 0; i < all.length ; i++){
+        //     str += '("' + all[i].name + '",' + all[i].lat + ',' + all[i].lng + ',"' + all[i].description + '",' + all[i].a_lots + ',' + all[i].t_lots + ',"' + all[i].pin + '",' + all[i].price + '), \n';
+        //   }
+        //   console.log(str);
+        // });
         //initializing and drawing the map on the document
         configMap(ctrls);
 
